aa802681d7b9e156e4907b3dc6a4c8a1e25c1bbb
ItsmeLee <arsmagica90@gmail.com>
2020-10-11 17:12:07 +0900

    Update index.js file
    Add the function to check if url starts with http.
    if the url start with http, continue the function with the url
    if not, add the http to the url.
    And get request from the url,
    if error is exist, consolo.log the error and res.send Down!.
    if error is not exist and response status code less than and under to 445, response send up!
    If not, response send Down!.

--- src/index.js
+++ src/index.js
@@ -5,5 +5,30 @@ const app = express();
 
 const PORT = 4000;
 
+const getUrl = (req, res) => {
+  const {
+    query: { url },
+  } = req;
+  console.log(url);
+  request(
+    url.startsWith("http") ? url : `http://${url}`,
+    (error, response, body) => {
+      if (error) {
+        console.log(error);
+        res.send("Down!");
+        return;
+      } else {
+        if (response.statusCode <= 445) {
+          res.send("Up!");
+        } else {
+          res.send("Down!");
+        }
+      }
+    }
+  );
+};
+
+app.get("/", getUrl);
+
 // Codesanbox does not need PORT :)
 app.listen(PORT, () => console.log(`Listening!`));
